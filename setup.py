544f330c75fd896bdb7f5b5df812501d33f47a58
Lucas Windhager <lucas.windhager+github@gmail.com>
2020-09-02 14:08:57 +0100

    changing setup.py tf versions

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ REQUIRED_PACKAGES = [
     'psutil==5.7.0',
     'memoized-property==1.0.3',
     'scipy== 1.4.1',
-    'tensorflow==2.3.0',
+    'tensorflow==2.0.0',
     'tensorflow-hub==0.9.0',
-    'tf-models-official==2.3.0',
+    'tf-models-official==2.0.0',
     'streamlit==0.64.0']
 
 setup(
